1ce496f36c2acc29d03346065e13a07c8f3992ee
Rimuru <Rimuru.Dev@gmail.com>
2024-02-26 19:14:44 +0400

    Regenerate .meta for SplashScreenModifier

--- SplashScreenModifier.cs
+++ SplashScreenModifier.cs
@@ -12,13 +12,14 @@
 // **************************************************************** //
 
 #if UNITY_EDITOR
+using UnityEngine;
 using UnityEditor;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
 
-public class SplashScreenModifier : IPreprocessBuildWithReport
+public sealed class SplashScreenModifier : IPreprocessBuildWithReport
 {
-    public int callbackOrder { get { return 0; } }
+    public int callbackOrder => 0;
 
     public void OnPreprocessBuild(BuildReport report)
     {
