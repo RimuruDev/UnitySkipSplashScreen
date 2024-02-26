7af1755ad5fbf272a23fb85fce9636d04b497320
Rimuru <85500556+RimuruDev@users.noreply.github.com>
2024-02-26 19:05:51 +0400

    Deleting the original splash screen sprite

--- SetSplashScreenBlack.cs
+++ SetSplashScreenBlack.cs
@@ -13,15 +13,23 @@
 
 #if UNITY_EDITOR
 using UnityEditor;
-using UnityEngine;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
 
-public sealed class SetSplashScreenBlack : IPreprocessBuildWithReport
+public class SplashScreenModifier : IPreprocessBuildWithReport
 {
     public int callbackOrder { get { return 0; } }
 
     public void OnPreprocessBuild(BuildReport report)
+    {
+        RemoveAllLogos();
+        SetSplashScreenBackground();
+    }
+
+    private static void RemoveAllLogos() =>
+        PlayerSettings.SplashScreen.logos = new PlayerSettings.SplashScreenLogo[0];
+
+    private static void SetSplashScreenBackground()
     {
         PlayerSettings.SplashScreen.unityLogoStyle = PlayerSettings.SplashScreen.UnityLogoStyle.LightOnDark;
         PlayerSettings.SplashScreen.backgroundColor = Color.black;
